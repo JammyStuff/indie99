5dc6518b7f0845c31bac8991712502129a3778f6
James Wheatley <james@jammy.co>
2016-01-16 23:15:47 +0000

    Added posts index

--- src/controller/indie99_posts_controller.erl
+++ src/controller/indie99_posts_controller.erl
@@ -1,5 +1,10 @@
 -module(indie99_posts_controller, [Req, SessionID]).
--export([view/2]).
+-export([index/2, view/2]).
+
+index('GET', []) ->
+    Posts = boss_db:find(post, [],
+                               [{order_by, published_at}, {descending, true}]),
+    {ok, [{title, "Posts"}, {posts, Posts}]}.
 
 view('GET', [PostSlug]) ->
     case boss_db:find_first(post, [slug, equals, PostSlug]) of
